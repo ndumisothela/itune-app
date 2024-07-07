ab99a0955c1b54877f49bd382f6db11fcebdc9bb
Ndumiso Thela <ndumisothela480@gmail.com>
2024-07-07 03:35:52 +0200

    server ready

--- my-itune/src/App.test.js
+++ my-itune/src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// App.test.js
+jest.mock('axios'); // Example if you need to mock Axios
+
+const { render, screen } = require('@testing-library/react');
+const App = require('./App').default; // Use .default if you're exporting as default
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
